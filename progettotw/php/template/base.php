157004f415278b4ee400d58ae3140f7019983fac
sofiabagagli <sofia.bagagli@studio.unibo.it>
2021-12-30 18:21:49 +0100

    modify base.php

--- progettotw/php/template/base.php
+++ progettotw/php/template/base.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap & Bootstrap Icon,  CSS -->
-   link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -14,24 +14,7 @@
     <link href="css/chatbot-style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://unpkg.com/botui/build/botui.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/botui/build/botui-theme-default.css" />
-    <!-- Titolo Pagina -->
-<<<<<<< HEAD
-    <?php
-      if(isset($templateParams["js"])):
-          foreach($templateParams["js"] as $script):
-      ?>
-          <script src="<?php echo $script; ?>"></script>
-      <?php
-          endforeach;
-      endif;
-      ?>
 
-=======
-    <script type="text/javascript" src="js/function.js"></script>
-    <link href="css/chatbot-style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://unpkg.com/botui/build/botui.min.css" />
-    <link rel="stylesheet" href="https://unpkg.com/botui/build/botui-theme-default.css" />
->>>>>>> 84fe02bbce54178911c08efc45d5586ea409d6b0
     <!-- Titolo Pagina -->
     <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
