84fe02bbce54178911c08efc45d5586ea409d6b0
Elisa Simoni <simoni.elisa00@gmail.com>
2021-12-30 17:18:51 +0100

    add bot

--- progettotw/php/template/base.php
+++ progettotw/php/template/base.php
@@ -11,7 +11,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <script type="text/javascript" src="js/function.js"></script>
-
+    <link href="css/chatbot-style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://unpkg.com/botui/build/botui.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/botui/build/botui-theme-default.css" />
     <!-- Titolo Pagina -->
     <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
@@ -123,6 +125,9 @@
         <!-- Grid row -->
         </div>
     </section>
+    <div class="botui-app-container" id="ecobot">
+        <bot-ui></bot-ui>
+    </div>
     <!-- Section: Links  -->
 
     <!-- Copyright -->
@@ -132,7 +137,9 @@
     <!-- Copyright -->
     </footer>
     <!-- Footer -->
-
+    <script src="https://cdn.jsdelivr.net/vue/2.0.5/vue.min.js"></script>
+    <script src="https://unpkg.com/botui/build/botui.js"></script>
+    <script src="js/chatbot.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
