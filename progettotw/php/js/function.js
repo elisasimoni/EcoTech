a6c6c2a83e01fe79e95715a9139d7bdc1fe9e61f
yuricollini36 <43495892+yuricollini36@users.noreply.github.com>
2022-01-03 16:49:12 +0100

    Add files via upload

--- progettotw/php/js/function.js
+++ progettotw/php/js/function.js
@@ -1,14 +1,59 @@
 $(document).ready(function(){
+  
+  $("button#registrati").click(function(){
 
-  $("input#floatingCompanyName, input#floatingPIVA").hide();
 
-  $("input#pf").click(function(){
-    $("input#floatingName, input#floatingSurname, input#floatingCF").show();
-    $("input#floatingCompanyName, input#floatingPIVA").hide();
+    
   });
-  $("input#pg").click(function(){
-      $("input#floatingName, input#floatingSurname, input#floatingCF").hide();
-      $("input#floatingCompanyName, input#floatingPIVA").show();
-    });
+
+
+  $("#registrati").click(function(e) {
+
+    
+    e.preventDefault();
+    var form = $("#registrazioneForm")
+
+    if($("input#nome").val()==""){
+      $("input#nome").addClass('is-invalid');
+    }else{
+      $("input#nome").addClass('is-valid');
+    }
+
+    if($("input#cognome").val()==""){
+      $("input#cognome").addClass('is-invalid');
+    }else{
+      $("input#cognome").addClass('is-valid');
+    }
+
+    if(($("input#cf").val().length == 11 ) || ($("input#cf").val().length == 16)){
+      $("input#cf").addClass('is-valid')
+    }else{
+      $("input#cf").addClass('is-invalid');
+    }
+
+    if(($("input#telefono").val().length < 9) || ($("input#telefono").val().length > 15)){
+      $("input#telefono").addClass('is-invalid');
+    }else{
+      $("input#telefono").addClass('is-valid');
+    }
+
+    if($("input#email").val().includes("@")){
+      $("input#email").addClass('is-valid');
+    }else{
+      $("input#email").addClass('is-invalid');
+    }
+
+    if( ($("input#password").val().length >= 8 ) && ($("input#password").val().match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) && ($("input#password").val().match(/([0-9])/)) && ($("input#password").val().match(/([!,%,&,@,#,$,^,*,?,_,~])/)) ) {
+      $("input#password").addClass('is-valid');
+    }else{
+      $("input#password").addClass('is-invalid');
+      $("div#passError").html(function(){
+        '<p>Password Brutta</p>';
+      });
+    }
+
+    //Make ajax call here
+
+  })
 
 });
