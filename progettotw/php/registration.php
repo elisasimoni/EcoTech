2df82201768172545fdc80ab05339ae6c70e60e8
yuricollini36 <43495892+yuricollini36@users.noreply.github.com>
2022-01-03 16:48:47 +0100

    Add files via upload

--- progettotw/php/registration.php
+++ progettotw/php/registration.php
@@ -1,8 +1,9 @@
 <?php
 require_once("bootstrap.php");
 
-$templateParams["titolo"] = "Eco-Tech | Login";
+$templateParams["titolo"] = "Eco-Tech | Registrazione";
 $templateParams["contenuto"] = "registration-form.php";
+
 
 require("template/base-registration.php");
 ?>
