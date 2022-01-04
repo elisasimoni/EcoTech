3b32d883a734cb46eff248faa03112e133aa9d35
yuricollini36 <43495892+yuricollini36@users.noreply.github.com>
2022-01-05 00:21:00 +0100

    Add files via upload

--- progettotw/php/utils/function.php
+++ progettotw/php/utils/function.php
@@ -1,7 +1,5 @@
 <?php
 
-//Funzione da implementare per aggiungere la classe active quando si seleziona un link da menù di navigazione
-//attenzione : non devono essere sovrascritte ulteriori classi già presenti. La classe active deve essere aggiunta come ulteriore
 function isActive($pagename, $classStart){
     if(basename($_SERVER['PHP_SELF'])==$pagename){
         echo "class=' $classStart active'";
