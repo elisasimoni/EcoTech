2fd4f99ac4cf41f43a246d8cc87dbf6b800e0a65
Elisa Simoni <73821477+xeli00@users.noreply.github.com>
2022-01-25 23:08:40 +0100

    Delete gestisci-registrazione.php

--- progettotw/php/gestisci-registrazione.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("bootstrap.php");
-
-$templateParams["titolo"] = "Eco-Tech | User-Registration";
-$templateParams["contenuto"] = "registration-form.php";
-require("template/base-registration.php");
-?>
