8170def69dc0be64ac545717fee5afa95e614666
Elisa Simoni <73821477+xeli00@users.noreply.github.com>
2022-01-25 23:07:21 +0100

    Add files via upload

--- progettotw/php/utils/function.php
+++ progettotw/php/utils/function.php
@@ -22,6 +22,11 @@ function dimQuantita($quantità){
   return $quantità;
 }
 
+function dimQuantitaMagazzino($quantitàM, $quantitàC){
+  $quantità = $quantitàM - $quantitàC;
+  return $quantità;
+}
+
 function sommaPrezzo($prezzo, $quantità){
   $totPrezzo = 0;
   for($i=0; $i < $quantità; $i++){
@@ -44,6 +49,115 @@ function isEmpty($quantità){
   }
 }
 
+function totPrezzo($prezzo){
+  if($prezzo == 0){
+    echo "00";
+  }else{
+    echo $prezzo;
+  }
+}
+
+function totPrzSpe($prezzo, $spedizione){
+  $totPrezzo = $prezzo + $spedizione;
+  return $totPrezzo;
+}
+
+function statoMagazzino($stato){
+  if($stato == "Disponibile"){
+    echo " bi bi-check2 text-success fw-bold";
+  }else if($stato == "presto disponibile"){
+    echo " bi-x text-warning fw-bold";
+  }else if($stato == "esaurito"){
+    echo " bi-x text-danger fw-bold";
+  }
+}
+
+function codiceAlfanumerico(){
+   $N_Caratteri = 10;
+   $Stringa = "";
+   For($I=0;$I<$N_Caratteri;$I++){
+     do{
+       $N = Ceil(rand(48,122));
+     }while(!((($N >= 48) && ($N <= 57)) || (($N >= 65) && ($N <= 90)) || (($N >= 97) && ($N <= 122))));
+     $Stringa = $Stringa.Chr ($N);
+   }
+   return $Stringa;
+}
+//funzione azione da svolgere
+function azione($azione){
+  if($azione == "1"){
+    echo "Inserisci Nuovo Prodotto";
+  }else if($azione == "2" || $azione == "4"){
+    echo "Modifica Prodotto";
+  }else if($azione == "3"){
+    echo "Elimina Prodotto";
+  }
+}
+
+function quantita($quantita){
+  if($quantita == "0"){
+    echo "btn-secondary disabled";
+  }else{
+    echo "btn-success border-0";
+  }
+}
+
+function somma($prezzo){
+  if(!$prezzo){
+    echo "disabled";
+  }else{
+    echo "";
+  }
+}
+
+function puntiEco($punti){
+  $puntiMax = "6";
+  $puntiNonAssegnati = $puntiMax - $punti;
+  for($i=0; $i < $punti; $i++){
+    echo "<li class='list-inline-item me-0 medium'><i class='bi bi-recycle text-success'></i></li>";
+  }
+  for($i=0; $i < $puntiNonAssegnati; $i++){
+    echo "<li class='list-inline-item me-0 medium'><i class='bi bi-recycle'></i></li>";
+  }
+}
+
+function sommaPunti($punti, $punteggio){
+    $totPunti = $punti + $punteggio;
+    return $totPunti;
+}
+
+function notifica($nome, $cognome, $data, $codice, $stato){
+  if($stato == "acquisto"){
+    return "Gentile $nome $cognome, l'ordine da lei effettuato è andato a buon fine!
+            In data e ora: $data
+            il suo codice identificativo dell'ordine:
+            $codice.";
+  }elseif($stato == "spedito"){
+    return "Gentile $nome $cognome, il suo ordine $codice, è stato $stato.";
+  }
+
+}
+
+function notificaVend($nome, $cognome, $data, $codice, $stato, $prodotto){
+  if($stato == "acquisto"){
+    return "Il cliente, $nome $cognome, ha effettuato un ordine!
+            In data e ora: $data
+            codice ordine:
+            $codice.";
+  }else if($stato == "esaurito"){
+    return "il prodotto, $prodotto, è esaurito! Affrettati a ricaricare il magazzino! ";
+  }
+
+}
+
+function checkLogin(){
+  if(isset($_SESSION["idCLIENTE"])){
+    echo "dash-utente.php";
+  }else{
+    echo "login.php";
+  }
+}
+
 function isUserLoggedIn(){
     return !empty($_SESSION['idCLIENTE']);
 }
@@ -53,74 +167,4 @@ function registerLoggedUser($user){
     $_SESSION["email"] = $user["email"];
     $_SESSION["nome"] = $user["nome"];
 }
-<<<<<<< HEAD
-
-function getAction($action){
-    $result = "";
-    switch($action){
-        case 1:
-            $result = "Inserisci";
-            break;
-        case 2:
-            $result = "Modifica";
-            break;
-        case 3:
-            $result = "Cancella";
-            break;
-    }
-
-    return $result;
-}
-
-
-function uploadImage($path, $image){
-    $imageName = basename($image["name"]);
-    $fullPath = $path.$imageName;
-
-    $maxKB = 500;
-    $acceptedExtensions = array("jpg", "jpeg", "png", "gif");
-    $result = 0;
-    $msg = "";
-    //Controllo se immagine è veramente un'immagine
-    $imageSize = getimagesize($image["tmp_name"]);
-    if($imageSize === false) {
-        $msg .= "File caricato non è un'immagine! ";
-    }
-    //Controllo dimensione dell'immagine < 500KB
-    if ($image["size"] > $maxKB * 1024) {
-        $msg .= "File caricato pesa troppo! Dimensione massima è $maxKB KB. ";
-    }
-
-    //Controllo estensione del file
-    $imageFileType = strtolower(pathinfo($fullPath,PATHINFO_EXTENSION));
-    if(!in_array($imageFileType, $acceptedExtensions)){
-        $msg .= "Accettate solo le seguenti estensioni: ".implode(",", $acceptedExtensions);
-    }
-
-    //Controllo se esiste file con stesso nome ed eventualmente lo rinomino
-    if (file_exists($fullPath)) {
-        $i = 1;
-        do{
-            $i++;
-            $imageName = pathinfo(basename($image["name"]), PATHINFO_FILENAME)."_$i.".$imageFileType;
-        }
-        while(file_exists($path.$imageName));
-        $fullPath = $path.$imageName;
-    }
-
-    //Se non ci sono errori, sposto il file dalla posizione temporanea alla cartella di destinazione
-    if(strlen($msg)==0){
-        if(!move_uploaded_file($image["tmp_name"], $fullPath)){
-            $msg.= "Errore nel caricamento dell'immagine.";
-        }
-        else{
-            $result = 1;
-            $msg = $imageName;
-        }
-    }
-    return array($result, $msg);
-}
 ?>
-=======
-?>
->>>>>>> 728175f9df16b8a881ebfd4aa38fa7f5eb079f31
