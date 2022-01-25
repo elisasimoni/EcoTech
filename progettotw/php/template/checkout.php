017dac9484ef7e1d5c645a1bda27aa0401b93498
Elisa Simoni <73821477+xeli00@users.noreply.github.com>
2022-01-25 23:06:03 +0100

    Delete checkout.php

--- progettotw/php/template/checkout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("bootstrap.php");
-
-$templateParams["titolo"] = "Eco-Tech | Checkout";
-$templateParams["contenuto"] = "checkout-form.php";
-
-require("template/base-checkout.php");
-?>
