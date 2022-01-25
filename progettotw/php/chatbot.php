133f3494b33c74125192e9da36ea4a786f1ba192
Elisa Simoni <73821477+xeli00@users.noreply.github.com>
2022-01-25 23:08:23 +0100

    Delete chatbot.php

--- progettotw/php/chatbot.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("bootstrap.php");
-
-$templateParams["titolo"] = "Eco-Tech | ChatBot";
-$templateParams["contenuto"] = "chat-bot.php";
-$templateParams["quantita"] = $dbh->getQuantitaProduct();
-require("template/base.php");
-?>
